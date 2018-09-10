ec5a7a99a66fd681687ef811690834ef590df8f4
hjtaki <hjtaki@gmail.com>
2018-09-10 09:08:26 -0700

    phone

---
+++ Lab4/Lab4/Phone.h
@@ -0,0 +1,16 @@
+//
+//  Phone.h
+//  Lab4
+//
+//  Created by Alex Jintak Han on 2018-09-10.
+//  Copyright © 2018 AlexHan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Phone : NSObject
+
+@property (nonatomic, strong) NSMutableArray *type;
+
+
+@end
---
+++ Lab4/Lab4/Phone.m
@@ -0,0 +1,22 @@
+//
+//  Phone.m
+//  Lab4
+//
+//  Created by Alex Jintak Han on 2018-09-10.
+//  Copyright © 2018 AlexHan. All rights reserved.
+//
+
+#import "Phone.h"
+
+@implementation Phone
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        <#statements#>
+    }
+    return self;
+}
+
+@end
